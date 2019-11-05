0a59fd4209effec2e16ef47c0686baf95519c235
Paul <pcmprieto@gmail.com>
2019-11-05 20:39:57 +0800

    Replaced classes of User Request and Vendor model.

--- app/User_request.php
+++ app/User_request.php
@@ -5,11 +5,26 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Vendor extends Model
+class User_request extends Model
 {
+
     use SoftDeletes;
+    public function status(){
+    	return $this->belongsTo('App\Status');
+    }
+
+    public function user(){
+    	return $this->belongsTo('App\User');
+    }
+
+    public function category(){
+    	return $this->belongsTo('App\Category');
+    }
 
     public function assets(){
-        return $this->hasMany('App\Asset');
+    	return $this->belongsToMany('App\Assets', 'asset_request')
+    		->withPivot('quantity')
+    		->withTimestamps();
     }
+
 }
--- app/Vendor.php
+++ app/Vendor.php
@@ -1,10 +1,16 @@
+
 <?php
 
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Vendor extends Model
 {
-    //
+    use SoftDeletes;
+
+    public function assets(){
+        return $this->hasMany('App\Asset');
+    }
 }
