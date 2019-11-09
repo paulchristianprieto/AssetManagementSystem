df53ce2fac93c252b7f8a77a6c9b8bb1d668c328
Paul <pcmprieto@gmail.com>
2019-11-09 20:45:32 +0800

    Added request information in table format

--- resources/views/user_requests/index.blade.php
+++ resources/views/user_requests/index.blade.php
@@ -44,7 +44,7 @@
 										<div class="card shadow bg-white rounded">
 											<div class="card-header" id="headingOne">
 												<div class="row">
-													<div class="{{ (Auth::user()->role_id === 2) ? "col-12": "col-9"}} ">
+													<div class="{{ (Auth::user()->role_id === 2) ? "col-12": "col-12"}} ">
 														<button 
 															class="btn btn-block text-left" 
 															type="button" 
@@ -65,54 +65,55 @@
 														</button>
 													</div>
 													
-													@can('isAdmin')
+													{{-- @can('isAdmin')
 														<div class="col-3 text-center">
 															<span>
-																<a href="{{ route('request_assign', ['user_request'=>$request->id]) }}" class="btn btn-dark btn-raised bg-info">Approve</a>
+																<a href=" {{ route('user_requests.show', ['user_request'=>$request->id]) }} " class="btn btn-success btn-outline-dark">View</a>
+																<a href="{{ route('request_assign', ['user_request'=>$request->id]) }}" class="btn btn-info btn-outline-dark" >Approve</a>
 															</span>
 														</div>
-													@endcan
+													@endcan --}}
 												</div>
 											</div>
 
 											<div id="request-{{$request->id }}" class="collapse" aria-labelledby="headingOne" data-parent="#requestsAccordion">
-												<div class="card-body">
-													<h5 class="card-title text-center">Summary</h5>
-													<div class="table-responsive mb-3">
-
-														<table class="table table-sm table-borderless">
+												<div class="card-body ">
+													{{-- Table --}}
+													<div class="table-responsive table-bordered table-striped">
+														<table class="table">
+															<thead class="thead-dark">
+																<tr>
+																	<th scope="col"><strong>Request</strong></th>
+																	<th scope="col">User</th>
+																	<th scope="col">Description</th>
+																	<th scope="col">Borrow Date</th>
+																	<th scope="col">Return Date</th>
+																	<th scope="col">Category</th>
+																	<th scope="col">Quantity</th>
+																	<th scope="col">Status</th>
+																	<th scope="col">Action</th>
+																</tr>
+															</thead>
 															<tbody>
 																<tr>
-																	<td>User Name:</td>
-																	<td><strong>{{$request->user->name }}</strong></td>
-																</tr>
-																<tr>
-																	<td>Request Number:</td>
-																	<td><strong>{{ $request->request_number }}</strong></td>
-																</tr>
-																<tr>
-																	<td>Status</td>
-																	<td> <span class="badge-warning badge">{{ $request->status->name }}</span></td>
-																</tr>
-																<tr>
-																	<td>Date</td>
-																	<td>{{$request->created_at->format('F d, Y')}}</td>
-																</tr>
-																<tr>
-																	<td>Total</td>
-																	<td> &#8369; {{ number_format($request->total, 2) }} </td>
-																</tr>
-															</tbody>
-															<tfoot>
-																<tr>
-																	<td colspan="2">
-																			{{-- {{dd($request->id) }} --}}
-																		<a href=" {{ route('user_requests.show', ['user_request'=>$request->id]) }} " class="page-link text-center rounded">View Details</a>
+																	<th scope="row">{{$request->request_number}} </th>
+																	<td>{{$request->user->name}}</td>
+																	<td>{{$request->description}}</td>
+																	<td>{{$request->borrow_date}}</td>
+																	<td>{{$request->return_date}}</td>
+																	<td>{{$request->category->name}}</td>
+																	<td>{{$request->quantity}}</td>
+																	<td> <p class="badge badge-warning ">{{$request->status->name}}</p></td>
+																	<td> 
+																		<a href=" {{ route('user_requests.show', ['user_request'=>$request->id]) }} " class="btn btn-success btn-block btn-outline-dark">View</a>
+																		<a href="{{ route('request_assign', ['user_request'=>$request->id]) }}" class="btn btn-info btn-block btn-outline-dark" >Approve</a>
 																	</td>
 																</tr>
-															</tfoot>
 														</table>
-
+													</div>
+													{{--  --}}
+													<div class="my-3">
+														<h3 class="text-center">Assets Available</h3>
 													</div>
 												</div>
 											</div>
